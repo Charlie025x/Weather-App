b5d93608b558d03291f7b13353fc3cd6fa3b0314
Charlie025x <charliealonso2001@gmail.com>
2023-06-16 11:57:27 -0500

    added basic markup, css, sample weather data, and a basic template for displaying data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { edinburgWeather } from "./call";
+
+import Day from "./components/Day";
 
 function App() {
+  /* filters data to show 1 weather report per day */
+
+  const filteredArr = edinburgWeather.list.filter(function (value, index, Arr) {
+    return index % 8 === 0;
+  });
+
+  /* ********************************************** */
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <i class="fa-solid fa-magnifying-glass fa-flip-horizontal"></i>
+        <i class="fa-solid fa-location-arrow"></i>Edinburg, TX
+        <i class="fa-solid fa-user"></i>
       </header>
+      <section>
+        <div className="weather-forcast">
+          <div className="forcast-heading">
+            <h2>5 day weather</h2>
+          </div>
+          {/* {edinburgWeather.list.map((data) => {
+            return <p>{data.dt_txt}</p>;
+          })}
+          {edinburgWeather.list[0].dt_txt} */}
+          {filteredArr.map((data) => {
+            return <p>{data.dt_txt}</p>;
+          })}
+
+          <Day />
+          {/* filters data to show 1 weather report per day */}
+          {edinburgWeather.list
+            .filter(function (value, index, Arr) {
+              return index % 8 === 0;
+            })
+            .map((data) => {
+              return <p>{data.dt_txt}</p>;
+            })}
+          {/* ********************************************** */}
+        </div>
+      </section>
     </div>
   );
 }
---
+++ src/components/Day.jsx
@@ -0,0 +1,19 @@
+const Day = () => {
+  return (
+    <div className="day">
+      <p>today</p>
+      <p>98.87&deg;</p>
+      <div>
+        <i class="fa-solid fa-cloud"></i>
+        <p>partly cloudy</p>
+      </div>
+      <div>
+        <i class="fa-solid fa-wind"></i>
+        <p>16 mph</p>
+      </div>
+      <i class="fa-solid fa-chevron-down"></i>
+    </div>
+  );
+};
+
+export default Day;
